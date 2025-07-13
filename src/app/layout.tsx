c41b5e8111b67173e32d2c637b7c39b476edab58
zico-hubb <mosesszico@gmail.com>
2025-07-13 11:19:47 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,23 @@
 
 import React from "react";
 import "./globals.css";
-import "../public/bootstrap/css/bootstrap-grid.min.css"; // ✅ Import bootstrap correctly
 
 export default function RootLayout(props: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
+      <head>
+        {/* ✅ Bootstrap CDN */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-qkU18z3+H3zJ1qVU+H9ZJ+I4hvYhJmVQYLPmddz0bYtPC2SnKiVRvVZG6H0Lz8z7"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="body">
-        <div>
-          {props.children}
-        </div>
+        {props.children}
       </body>
     </html>
   );
