52e318aef413968b59a5cdea86c4b24a2deb27a0
zico-hubb <mosesszico@gmail.com>
2025-07-13 11:25:52 +0300

    Update useProvider.tsx

--- src/lib/useProvider.tsx
+++ src/lib/useProvider.tsx
@@ -1,19 +1,1 @@
-// src/lib/useProvider.tsx
 
-'use client'
-
-import { ethers } from 'ethers';
-import { ExternalProvider } from '@ethersproject/providers';
-
-declare global {
-  interface Window {
-    ethereum?: ExternalProvider;
-  }
-}
-
-export function getProvider() {
-  if (!window.ethereum) {
-    throw new Error("❌ MetaMask not found");
-  }
-  return new ethers.BrowserProvider(window.ethereum);
-}
